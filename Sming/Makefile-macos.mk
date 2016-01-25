f6137108b7ae6ba219c917411cbab936b3dd1c25
Hukuma23 <nikita.litvinov@gmail.com>
2016-01-25 17:41:32 +0300

    Начало Sming git

---
+++ Sming/Makefile-macos.mk
@@ -0,0 +1,18 @@
+# ESP8266 sdk package home directory
+ESP_HOME ?= /opt/esp-open-sdk
+
+# Default COM port
+COM_PORT     ?= /dev/tty.usbserial-A50285BI
+
+# base directory of the ESP8266 SDK package, absolute
+SDK_BASE	?= $(ESP_HOME)/sdk
+SDK_TOOLS	 ?= $(SDK_BASE)/tools
+
+# Other tools mappings
+ESPTOOL		 ?= $(ESP_HOME)/esptool/esptool.py
+KILL_TERM    ?= pkill -9 -f "$(COM_PORT) $(COM_SPEED_SERIAL)" || exit 0
+GET_FILESIZE ?= stat -L -f%z
+#TERMINAL     ?= python -m serial.tools.miniterm $(COM_PORT) $(COM_SPEED_SERIAL)
+#TERMINAL     ?= python -m serial.tools.miniterm /dev/tty.usbserial-A50285BI 115200
+TERMINAL     ?= echo "done"
+MEMANALYZER  ?= $(OBJDUMP) -h -j .data -j .rodata -j .bss -j .text -j .irom0.text
