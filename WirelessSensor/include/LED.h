1654901a5c720c8ee4ea380d66ee91f5d163d931
Hukuma23 <nikita.litvinov@gmail.com>
2016-02-05 14:53:44 +0300

    LED.h: исключены констаны цветов из define в функции отображения цвета добавлены параметр яркость, значение по умолчанию определено в #define BRIGHT

--- WirelessSensor/include/LED.h
+++ WirelessSensor/include/LED.h
@@ -11,29 +11,38 @@
 #ifndef INCLUDE_LED_H_
 #define INCLUDE_LED_H_
 
-#define RED 	"\x40\x00\x00"
-#define GREEN 	"\x00\x40\x00"
-#define BLUE 	"\x00\x00\x40"
+
+#define BRIGHT	0x10
+
+
+#define RED 	"\x20\x00\x00"
+#define GREEN 	"\x00\x20\x00"
+#define BLUE 	"\x00\x00\x20"
 #define BLACK 	"\x00\x00\x00"
-#define WHITE 	"\x40\x40\x40"
+#define WHITE 	"\x20\x20\x20"
 
-void static ledRed(byte pin) {
-	ws2812_writergb(pin, (char*)RED, 3);
+void static ledRed(byte pin, byte bright = BRIGHT) {
+	char color[3] = {bright,0,0};
+	ws2812_writergb(pin, color, sizeof(color));
 }
 
-void static ledGreen(byte pin) {
-	ws2812_writergb(pin, (char*)GREEN, 3);
+void static ledGreen(byte pin, byte bright = BRIGHT) {
+	char color[3] = {0,bright,0};
+	ws2812_writergb(pin, color, sizeof(color));
 }
-void static ledBlue(byte pin) {
-	ws2812_writergb(pin, (char*)BLUE, 3);
+void static ledBlue(byte pin, byte bright = BRIGHT) {
+	char color[3] = {0,0,bright};
+	ws2812_writergb(pin, color, sizeof(color));
 }
 
 void static ledBlack(byte pin) {
-	ws2812_writergb(pin, (char*)BLACK, 3);
+	char color[3] = {0,0,0};
+	ws2812_writergb(pin, color, sizeof(color));
 }
 
-void static ledWhite(byte pin) {
-	ws2812_writergb(pin, (char*)WHITE, 3);
+void static ledWhite(byte pin, byte bright = BRIGHT) {
+	char color[3] = {bright,bright,bright};
+	ws2812_writergb(pin, color, sizeof(color));
 }
 
 #endif /* INCLUDE_LED_H_ */
