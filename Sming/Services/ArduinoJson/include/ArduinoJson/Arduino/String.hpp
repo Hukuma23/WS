84138d765ac9aa733bc5452800c76a2c397ad3aa
Hukuma23 <nikita.litvinov@gmail.com>
2016-05-17 19:09:52 +0300

    Correct commit for Sming library updates

---
+++ Sming/Services/ArduinoJson/include/ArduinoJson/Arduino/String.hpp
@@ -0,0 +1,12 @@
+// Copyright Benoit Blanchon 2014-2016
+// MIT License
+//
+// Arduino JSON library
+// https://github.com/bblanchon/ArduinoJson
+// If you like this project, please add a star!
+
+#pragma once
+
+
+#include <WString.h>
+
